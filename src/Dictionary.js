19ac21aba76cc2ddf825229d2db108bff22ebebf
Botiş Maria <126352756+maria-miruna@users.noreply.github.com>
2023-10-19 15:51:43 +0300

    added photos to dictionary

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 import "./Dictionary.css";
 
@@ -8,8 +9,9 @@ export default function Dictionary(props) {
   const [keyWord, setKeyWord] = useState(props.defaultKeyWord);
   const [result, setResult] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResult(response.data[0]);
   }
 
@@ -18,11 +20,20 @@ export default function Dictionary(props) {
     search();
   }
 
+  function handleSheCodesResponse(response) {
+    setPhotos(response.data.photos);
+  }
+
   function search() {
     // documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyWord}`;
 
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let sheCodesApiKey = `b3b90fo474e98fbe2dt0209a2baa7df2`;
+    let sheCodesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyWord}&key=${sheCodesApiKey}`;
+
+    axios.get(sheCodesApiUrl).then(handleSheCodesResponse);
   }
 
   function handleKeyWordChange(event) {
@@ -47,9 +58,10 @@ export default function Dictionary(props) {
               placeholder="Search for a word"
             />
           </form>
-          <div className="hint">i.e. sunset, wine, yoga, coding</div>
+          <div className="hint">i.e. summer, wine, yoga, coding</div>
         </section>
         <Results results={result} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
--- src/Phonetic.js
+++ src/Phonetic.js
@@ -3,11 +3,10 @@ import React from "react";
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  console.log(props.phonetic);
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="blank" rel="noreferrer">
-        <i class="fa-solid fa-volume-high"></i>
+        <i className="fa-solid fa-volume-high"></i>
       </a>{" "}
       <span className="text"> {props.phonetic.text} </span>
     </div>
